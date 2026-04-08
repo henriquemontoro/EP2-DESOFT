65fc79b713522990cb31c31328cab3f44752a506
Henrique <iquemontoro@gmail.com>
2026-04-08 10:47:40 -0300

    ex2

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,8 @@ def rolar_dados(quantidade):
         lista.append(random.randint(1, 6))
     
     return lista
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dados_no_estoque.append(dados_rolados[dado_para_guardar])
+    dados_rolados = dados_rolados[:dado_para_guardar] + dados_rolados[dado_para_guardar + 1:]
+    return [dados_rolados, dados_no_estoque]
