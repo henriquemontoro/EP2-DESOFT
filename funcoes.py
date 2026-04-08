3b6a75f3d5588b82bd7199e2bf75d0cf1a18a84f
Henrique <iquemontoro@gmail.com>
2026-04-08 11:34:35 -0300

    ex7

--- funcoes.py
+++ funcoes.py
@@ -46,3 +46,11 @@ def calcula_pontos_sequencia_baixa(dados):
         return 15
     else:
         return 0
+
+def calcula_pontos_sequencia_alta(dados):
+    if 1 in dados and 2 in dados and 3 in dados and 4 in dados and 5 in dados:
+        return 30
+    elif 2 in dados and 3 in dados and 4 in dados and 5 in dados and 6 in dados:
+        return 30
+    else:
+        return 0
